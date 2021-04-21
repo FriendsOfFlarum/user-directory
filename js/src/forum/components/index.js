afc48f0f2204bd1a5725b70c5b6adebe2bec7636
Rafał Całka <calka.rafal@gmail.com>
2021-04-21 13:58:24 +0200

    Export components
    Export components so they can be modified by other extensions

---
+++ js/src/forum/components/index.js
@@ -0,0 +1,15 @@
+import CheckableButton from './CheckableButton';
+import SearchField from './SearchField';
+import SmallUserCard from './SmallUserCard';
+import UserDirectoryList from './UserDirectoryList';
+import UserDirectoryListItem from './UserDirectoryListItem';
+import UserDirectoryPage from './UserDirectoryPage';
+
+export const components = {
+    CheckableButton,
+    SearchField,
+    SmallUserCard,
+    UserDirectoryList,
+    UserDirectoryListItem,
+    UserDirectoryPage,
+};
