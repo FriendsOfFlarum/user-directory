ecd60c4f668b02e392035886b6a72fdc61463284
IanM <ian@morland.me>
2025-02-16 11:53:15 +0000

    chore(2.0): JSON:API changes
    Flarum 2.0 completely changes the JSON:API implementation

--- extend.php
+++ extend.php
@@ -13,6 +13,10 @@ namespace FoF\UserDirectory;
 
 use Flarum\Api\Serializer\ForumSerializer;
 use Flarum\Extend;
+use Flarum\Api\Context;
+use Flarum\Api\Endpoint;
+use Flarum\Api\Resource;
+use Flarum\Api\Schema;
 
 return [
     (new Extend\Frontend('admin'))
@@ -25,6 +29,7 @@ return [
 
     new Extend\Locales(__DIR__.'/resources/locale'),
 
+    // @TODO: Replace with the new implementation https://docs.flarum.org/2.x/extend/api#extending-api-resources
     (new Extend\ApiSerializer(ForumSerializer::class))
         ->attributes(PermissionBasedForumSettings::class),
 
